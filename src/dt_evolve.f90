b314ec55377ce9e1f763f187a7d4ee94178d30a4
Shunsuke Sato <shunsuke.sato26@gmail.com>
2017-07-05 16:44:59 +0200

    Implemented omp parallelization.

--- src/dt_evolve.f90
+++ src/dt_evolve.f90
@@ -47,13 +47,15 @@ subroutine dt_evolve_Lanczos(ft)
   ft = 0d0
 
   ss = 0d0
+!$omp parallel do private(ix,iy) reduction(+:ss)
   do iy = 0,Nx
      do ix = 0,Nx
-        ss = ss + abs(zwfn(:,:))**2
+        ss = ss + abs(zwfn(ix,iy))**2
      end do
   end do
   ss = dx**2/sqrt(ss)
 
+!$omp parallel do private(ix,iy)
   do iy = 0,Nx
      do ix = 0,Nx
         zwfn(ix,iy) = zwfn(ix,iy) * ss
@@ -64,6 +66,7 @@ subroutine dt_evolve_Lanczos(ft)
   Hamiltonian_L = 0d0
 
   do j = 1,NLanczos
+!$omp parallel do private(ix,iy)
     do iy = 0,Nx
       do ix = 0,Nx
         ztmp_wfn(ix,iy) = zwfn_Lanczos(ix,iy,j)
@@ -72,9 +75,10 @@ subroutine dt_evolve_Lanczos(ft)
 
     call zhpsi(ztmp_wfn,ztmp_hwfn,ft)
     ss = 0d0
+!$omp parallel do private(ix,iy) reduction(+:ss)
     do iy = 0,Nx
        do ix = 0,Nx
-          ss = ss + conjg(ztmp_wfn(ix,iy))*ztmp_hwfn(ix,iy))
+          ss = ss + conjg(ztmp_wfn(ix,iy))*ztmp_hwfn(ix,iy)
        end do
     end do
 
@@ -82,12 +86,14 @@ subroutine dt_evolve_Lanczos(ft)
 
     if(j == NLanczos)exit
     if(j == 1) then
+!$omp parallel do private(ix,iy) 
       do iy = 0,Nx
         do ix = 0,Nx
           ztmp_wfn(ix,iy) = ztmp_hwfn(ix,iy) - Hamiltonian_L(j,j)*zwfn_Lanczos(ix,iy,j)
         end do
       end do
     else
+!$omp parallel do private(ix,iy) 
       do iy = 0,Nx
         do ix = 0,Nx
           ztmp_wfn(ix,iy) = ztmp_hwfn(ix,iy) &
@@ -98,6 +104,7 @@ subroutine dt_evolve_Lanczos(ft)
     end if
 
     ss = 0d0
+!$omp parallel do private(ix,iy) reduction(+:ss)
     do iy = 0,Nx
        do ix = 0,Nx
           ss = ss + abs(ztmp_wfn(ix,iy))**2
@@ -107,6 +114,7 @@ subroutine dt_evolve_Lanczos(ft)
     Hamiltonian_L(j,j+1) = ss
     Hamiltonian_L(j+1,j) = ss
 
+!$omp parallel do private(ix,iy) 
     do iy = 0,Nx
        do ix = 0,Nx
           zwfn_Lanczos(ix,iy,j+1) = ztmp_wfn(ix,iy)/ss
@@ -134,6 +142,7 @@ subroutine dt_evolve_Lanczos(ft)
   end do
   write(*,*)'norm =',sum(abs(zvec_t)**2)
 
+!$omp parallel do private(ix,iy) 
   do ix = 0,Nx
      do iy = 0,Nx
         zwfn(ix,iy) = zvec_t(1)*zwfn_Lanczos(ix,iy,1)
@@ -141,8 +150,9 @@ subroutine dt_evolve_Lanczos(ft)
   end do
 
   do j = 1,NLanczos
-     do ix = 0,Nx
-        do iy = 0,Nx
+!$omp parallel do private(ix,iy) 
+    do iy = 0,Nx
+      do ix = 0,Nx
            zwfn(ix,iy) = zwfn(ix,iy) + zvec_t(j)*zwfn_Lanczos(ix,iy,j)
         end do
      end do
