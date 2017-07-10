05f7398706f58d3283fbe133d447ec9f910de204
Shunsuke Sato <shunsuke.sato26@gmail.com>
2017-07-10 17:50:23 +0200

    calc occ nat.

--- src/RT_prop.f90
+++ src/RT_prop.f90
@@ -6,13 +6,16 @@
 subroutine RT_prop
   use global_variables
   use timer
+  use density_matrix
   implicit none
   real(dp) :: ft,dipole,norm
   integer :: it,ix
   character(50) :: citer
   real(8) :: time_ini,time_end,time_elapse
+  real(8) :: occ_nat(Nx+1)
 
   call elapse_time(time_ini)
+  call initialize_density_matrix
 
   write(*,'(A)')'===== Real time propagation ======================================================'
   write(*,'(A)')
@@ -34,6 +37,10 @@ subroutine RT_prop
   end do
   close(22)
 
+  open(45,file='occ_nat.out')
+  call calc_occ_natural_orbital(occ_nat)
+  write(45,"(999e26.16e3)")it*dt,occ_nat(1:100)
+
   write(citer,'(I7.7)')it
 
   do it = 1, Nt_iter
@@ -46,6 +53,11 @@ subroutine RT_prop
 !     call dipole_norm(dipole,norm)
 !     dipole_t(it) = dipole
 !     norm_t(it) = norm
+
+     if (mod(it,1000) == 0)then
+       call calc_occ_natural_orbital(occ_nat)
+       write(45,"(999e26.16e3)")it*dt,occ_nat(1:100)
+     end if
 
      if (mod(it,100) == 0 .or. it == 1)then
 !     if (1 == 0)then
@@ -66,6 +78,8 @@ subroutine RT_prop
   write(*,'(A)')
   write(*,'(A)')
   write(*,'(A)')'===== complete Real time propagation ============================================'
+
+  close(45)
 
   call elapse_time(time_end)
   time_elapse = time_end - time_ini
